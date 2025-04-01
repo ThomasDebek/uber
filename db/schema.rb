8f0fa83d0a8683540552378b962611b010696be5
ThomasDebek <tom.debek@gmail.com>
2025-04-01 20:27:14 +0200

    migration: change file

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_01_174920) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_01_182104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.text "descriptioin"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
+
+
+require 'faker'
+
+puts "Delete product all and generate new and User"
+Product.destroy_all
 User.destroy_all
-
-
-
-p "Generating new user"
-User.create(
-  email: "dave@gmail.com",
-  password: "secret"
-)
-p "done"
+User.create(email: "dave@gmail.com", password: "secret")
+10.times do
+  p = Product.create(
+    name: Faker::Commerce.unique.product_name,
+    price: Faker::Number.decimal(l_digits: 2),
+    )
+  puts "generating - product - #{p.name}"
+end

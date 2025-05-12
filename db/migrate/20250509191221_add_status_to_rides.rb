4f46506da10c8a0fa2adc395cddf750441e3f57d
ThomasDebek <tom.debek@gmail.com>
2025-05-12 18:52:16 +0200

    migrate: rollback and addd default 0 to riedes

--- db/migrate/20250509191221_add_status_to_rides.rb
+++ db/migrate/20250509191221_add_status_to_rides.rb
@@ -1,5 +1,5 @@
 class AddStatusToRides < ActiveRecord::Migration[8.0]
   def change
-    add_column :rides, :status, :integer
+    add_column :rides, :status, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_09_191221) do
     t.bigint "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status"
+    t.integer "status", default: 0
     t.index ["customer_id"], name: "index_rides_on_customer_id"
   end
 
